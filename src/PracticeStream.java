15078f9388013721c763691e8de3cf7c3d2c9616
Mohammad <waseem.razaa@gmail.com>
2026-03-25 11:49:47 +0100

    use of flatMap, groupBy, joining in Stream

--- src/PracticeStream.java
+++ src/PracticeStream.java
@@ -6,7 +6,12 @@ void main() {
     sortList();
     findFirst();
     findMax();
+    concatenateStrings();
     removeDuplicate();
+    convertListToMap();
+    groupByLength();
+    flatMap();
+    findSecondHighest();
 }
 
 private static void removeDuplicate() {
@@ -51,6 +56,42 @@ private static void convertToUppercase() {
     IO.println(result);
 }
 
+private static void convertListToMap() {
+    List<String> input = Arrays.asList("java", "stream", "api");
+    System.out.println("List: " + input);
+    Map<String, Integer> map = input.stream()
+            .collect(Collectors.toMap(
+                    s -> s, String::length
+            ));
+
+    IO.println("List To Map: " + map);
+}
+
+private static void groupByLength() {
+    List<String> input = Arrays.asList("java", "stream", "api", "Mohammad", "Raza");
+    System.out.println("List: " + input);
+    Map<Integer, List<String>> collect = input.stream()
+            .collect(Collectors.groupingBy(String::length));
+
+    IO.println("Group by length: " + collect);
+}
+
+private static void countNumberOfStrings() {
+    List<String> input = Arrays.asList("java", "stream", "api");
+    long count = input.stream()
+            .filter(s -> s.length() > 3)
+            .count();
+    IO.println("count = " + count);
+}
+
+private static void concatenateStrings() {
+    List<String> input = Arrays.asList("java", "stream", "api");
+    String result = input.stream()
+            .collect(Collectors.joining(", "));
+    // String result = String.join(", ", input); // without using Stream
+    IO.println("Concatenated strings: " + result);
+}
+
 private static void filterEvenNumbers() {
     List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
     List<Integer> result = numbers.stream()
@@ -67,10 +108,27 @@ private static void sumOfAllElements() {
     IO.println("sum = " + sum);
 }
 
-private static void countNumberOfStrings() {
-    List<String> input = Arrays.asList("java", "stream", "api");
-    long count = input.stream()
-            .filter(s -> s.length() > 3)
-            .count();
-    IO.println("count = " + count);
+private static void flatMap() {
+    List<List<Integer>> nested = Arrays.asList(
+            Arrays.asList(1, 2),
+            Arrays.asList(3, 4)
+    );
+    IO.println("Before flatmap: " + nested);
+    List<Integer> flat = nested.stream()
+            .flatMap(List::stream)
+            .toList();
+
+    IO.println("After flatmap: " + flat);
+}
+
+private static void findSecondHighest() {
+    List<Integer> numbers = Arrays.asList(5, 6, 2, 3, 12, 1);
+    IO.println("Initial List: " + numbers);
+    Optional<Integer> secondHighest = numbers.stream()
+            .distinct()
+            .sorted(Comparator.reverseOrder())
+            .skip(1)
+            .findFirst();
+
+    IO.println("Second highest: " + secondHighest.orElse(-1));
 }
